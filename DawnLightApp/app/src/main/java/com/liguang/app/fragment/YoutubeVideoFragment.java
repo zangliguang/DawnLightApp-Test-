869f5f3a6544e73794be57dd1523cbe7df03e396
Zang Liguang <zangliguang@baidu.com>
2015-10-22 15:28:09 +0800

    添加UltimateRecyclerView

--- DawnLightApp/app/src/main/java/com/liguang/app/fragment/YoutubeVideoFragment.java
+++ DawnLightApp/app/src/main/java/com/liguang/app/fragment/YoutubeVideoFragment.java
@@ -3,12 +3,31 @@ package com.liguang.app.fragment;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.liguang.app.R;
+import com.liguang.app.adapter.SimpleAnimationAdapter;
+import com.marshalchen.ultimaterecyclerview.CustomUltimateRecyclerview;
+import com.marshalchen.ultimaterecyclerview.ObservableScrollState;
+import com.marshalchen.ultimaterecyclerview.ObservableScrollViewCallbacks;
+import com.marshalchen.ultimaterecyclerview.UltimateRecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import in.srain.cube.views.ptr.PtrDefaultHandler;
+import in.srain.cube.views.ptr.PtrFrameLayout;
+import in.srain.cube.views.ptr.PtrHandler;
+import in.srain.cube.views.ptr.PtrUIHandler;
+import in.srain.cube.views.ptr.header.MaterialHeader;
+import in.srain.cube.views.ptr.header.StoreHouseHeader;
+import in.srain.cube.views.ptr.indicator.PtrIndicator;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -29,6 +48,41 @@ public class YoutubeVideoFragment extends Fragment {
     private String mParam2;
 
     private OnFragmentInteractionListener mListener;
+
+    CustomUltimateRecyclerview ultimateRecyclerView;
+    SimpleAnimationAdapter simpleRecyclerViewAdapter = null;
+    View floatingButton = null;
+    LinearLayoutManager linearLayoutManager;
+    int moreNum = 2;
+    protected Context mContext = null;
+
+    private int mLoadTime = 0;
+    StoreHouseHeader storeHouseHeader;
+    MaterialHeader materialHeader;
+    Handler changeHeaderHandler = new Handler() {
+
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case 0:
+                    refreshingStringArray();
+                    break;
+                case 1:
+                    //  refreshingMaterial();
+                    refreshingString();
+                    break;
+                case 2:
+                    // refreshingString();
+                    break;
+                case 3:
+                    refreshingString();
+                    break;
+                case 4:
+                    break;
+            }
+        }
+    };
 
     /**
      * Use this factory method to create a new instance of
@@ -65,7 +119,93 @@ public class YoutubeVideoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_youtube_video, container, false);
+        View rootview = inflater.inflate(R.layout.fragment_youtube_video, container, false);
+        initView(rootview);
+        return rootview;
+    }
+
+    private void initView(View rootview) {
+        ultimateRecyclerView = (CustomUltimateRecyclerview) rootview.findViewById(R.id.custom_ultimate_recycler_view);
+        ultimateRecyclerView.setHasFixedSize(false);
+        floatingButton = rootview.findViewById(R.id.custom_urv_add_floating_button);
+        List<String> stringList = new ArrayList<>();
+        simpleRecyclerViewAdapter = new SimpleAnimationAdapter(stringList);
+
+        stringList.add("111");
+        stringList.add("aaa");
+        stringList.add("222");
+        stringList.add("33");
+        stringList.add("44");
+        stringList.add("55");
+        stringList.add("66");
+        stringList.add("11771");
+        linearLayoutManager = new LinearLayoutManager(mContext);
+        ultimateRecyclerView.setLayoutManager(linearLayoutManager);
+        ultimateRecyclerView.setAdapter(simpleRecyclerViewAdapter);
+
+        ultimateRecyclerView.enableLoadmore();
+        simpleRecyclerViewAdapter.setCustomLoadMoreView(LayoutInflater.from(mContext)
+                .inflate(R.layout.custom_bottom_progressbar, null));
+
+        ultimateRecyclerView.setParallaxHeader(getActivity().getLayoutInflater().inflate(R.layout.parallax_recyclerview_header, ultimateRecyclerView.mRecyclerView, false));
+        ultimateRecyclerView.setOnParallaxScroll(new UltimateRecyclerView.OnParallaxScroll() {
+            @Override
+            public void onParallaxScroll(float percentage, float offset, View parallax) {
+
+//                Drawable c =getActivity().getActionBar().getCustomView().getBackground();
+//                c.setAlpha(Math.round(127 + percentage * 128));
+//                getActivity().getActionBar().getCustomView().setBackgroundDrawable(c);
+            }
+        });
+
+        ultimateRecyclerView.setOnLoadMoreListener(new UltimateRecyclerView.OnLoadMoreListener() {
+            @Override
+            public void loadMore(int itemsCount, final int maxLastVisiblePosition) {
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    public void run() {
+                        simpleRecyclerViewAdapter.insert("More " + moreNum++, simpleRecyclerViewAdapter.getAdapterItemCount());
+                        simpleRecyclerViewAdapter.insert("More " + moreNum++, simpleRecyclerViewAdapter.getAdapterItemCount());
+                        simpleRecyclerViewAdapter.insert("More " + moreNum++, simpleRecyclerViewAdapter.getAdapterItemCount());
+                        // linearLayoutManager.scrollToPositionWithOffset(maxLastVisiblePosition,-1);
+                        //   linearLayoutManager.scrollToPosition(maxLastVisiblePosition);
+
+                    }
+                }, 1000);
+            }
+        });
+        // ultimateRecyclerView.hideDefaultFloatingActionButton();
+        // ultimateRecyclerView.hideFloatingActionMenu();
+        ultimateRecyclerView.displayCustomFloatingActionView(false);
+        ultimateRecyclerView.setScrollViewCallbacks(new ObservableScrollViewCallbacks() {
+            @Override
+            public void onScrollChanged(int scrollY, boolean firstScroll, boolean dragging) {
+
+            }
+
+            @Override
+            public void onDownMotionEvent() {
+
+            }
+
+            @Override
+            public void onUpOrCancelMotionEvent(ObservableScrollState observableScrollState) {
+                if (observableScrollState == ObservableScrollState.DOWN) {
+                    //  ultimateRecyclerView.showToolbar(toolbar, ultimateRecyclerView, getScreenHeight());
+                    //  ultimateRecyclerView.showView(floatingButton, ultimateRecyclerView, getScreenHeight());
+                } else if (observableScrollState == ObservableScrollState.UP) {
+//                    ultimateRecyclerView.hideToolbar(toolbar, ultimateRecyclerView, getScreenHeight());
+//                    ultimateRecyclerView.hideFloatingActionMenu();
+                    //   ultimateRecyclerView.hideView(floatingButton, ultimateRecyclerView, getScreenHeight());
+
+                } else if (observableScrollState == ObservableScrollState.STOP) {
+                }
+            }
+        });
+        ultimateRecyclerView.setCustomSwipeToRefresh();
+
+        // refreshingMaterial();
+        refreshingString();
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -78,6 +218,7 @@ public class YoutubeVideoFragment extends Fragment {
     @Override
     public void onAttach(Context activity) {
         super.onAttach(activity);
+        mContext = activity;
         try {
             mListener = (OnFragmentInteractionListener) activity;
         } catch (ClassCastException e) {
@@ -97,7 +238,7 @@ public class YoutubeVideoFragment extends Fragment {
      * fragment to allow an interaction in this fragment to be communicated
      * to the activity and potentially other fragments contained in that
      * activity.
-     * <p/>
+     * <p>
      * See the Android Training lesson <a href=
      * "http://developer.android.com/training/basics/fragments/communicating.html"
      * >Communicating with Other Fragments</a> for more information.
@@ -105,6 +246,122 @@ public class YoutubeVideoFragment extends Fragment {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         public void onFragmentInteraction(Uri uri);
+    }
+
+
+    void refreshingString() {
+        storeHouseHeader = new StoreHouseHeader(mContext);
+        //   header.setPadding(0, 15, 0, 0);
+
+        storeHouseHeader.initWithString("Marshal Chen");
+        //  header.initWithStringArray(R.array.akta);
+        ultimateRecyclerView.mPtrFrameLayout.removePtrUIHandler(materialHeader);
+        ultimateRecyclerView.mPtrFrameLayout.setHeaderView(storeHouseHeader);
+        ultimateRecyclerView.mPtrFrameLayout.addPtrUIHandler(storeHouseHeader);
+        ultimateRecyclerView.mPtrFrameLayout.autoRefresh(false);
+        ultimateRecyclerView.mPtrFrameLayout.setPtrHandler(new PtrHandler() {
+            @Override
+            public boolean checkCanDoRefresh(PtrFrameLayout ptrFrameLayout, View view, View view2) {
+                boolean canbePullDown = PtrDefaultHandler.checkContentCanBePulledDown(ptrFrameLayout, view, view2);
+                return canbePullDown;
+            }
+
+            @Override
+            public void onRefreshBegin(PtrFrameLayout ptrFrameLayout) {
+                ptrFrameLayout.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        simpleRecyclerViewAdapter.insert("Refresh things", 0);
+                        //   ultimateRecyclerView.scrollBy(0, -50);
+                        linearLayoutManager.scrollToPosition(0);
+                        ultimateRecyclerView.mPtrFrameLayout.refreshComplete();
+                        changeHeaderHandler.sendEmptyMessageDelayed(0, 500);
+                    }
+                }, 1800);
+            }
+        });
+    }
+
+    void refreshingStringArray() {
+        storeHouseHeader = new StoreHouseHeader(mContext);
+        storeHouseHeader.setPadding(0, 15, 0, 0);
+
+        // using string array from resource xml file
+        storeHouseHeader.initWithStringArray(R.array.storehouse);
+
+        ultimateRecyclerView.mPtrFrameLayout.setDurationToCloseHeader(1500);
+        ultimateRecyclerView.mPtrFrameLayout.removePtrUIHandler(materialHeader);
+        ultimateRecyclerView.mPtrFrameLayout.setHeaderView(storeHouseHeader);
+        ultimateRecyclerView.mPtrFrameLayout.addPtrUIHandler(storeHouseHeader);
+
+
+        ultimateRecyclerView.mPtrFrameLayout.autoRefresh(false);
+//        ultimateRecyclerView.mPtrFrameLayout.postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                ultimateRecyclerView.mPtrFrameLayout.autoRefresh(false);
+//            }
+//        }, 100);
+
+        // change header after loaded
+        ultimateRecyclerView.mPtrFrameLayout.addPtrUIHandler(new PtrUIHandler() {
+
+
+            @Override
+            public void onUIReset(PtrFrameLayout frame) {
+                mLoadTime++;
+                if (mLoadTime % 2 == 0) {
+                    storeHouseHeader.setScale(1);
+                    storeHouseHeader.initWithStringArray(R.array.storehouse);
+                } else {
+                    storeHouseHeader.setScale(0.5f);
+                    storeHouseHeader.initWithStringArray(R.array.akta);
+                }
+            }
+
+            @Override
+            public void onUIRefreshPrepare(PtrFrameLayout frame) {
+
+            }
+
+            @Override
+            public void onUIRefreshBegin(PtrFrameLayout frame) {
+
+            }
+
+            @Override
+            public void onUIRefreshComplete(PtrFrameLayout frame) {
+
+            }
+
+            @Override
+            public void onUIPositionChange(PtrFrameLayout frame, boolean isUnderTouch, byte status, PtrIndicator ptrIndicator) {
+
+            }
+        });
+
+        ultimateRecyclerView.mPtrFrameLayout.setPtrHandler(new PtrHandler() {
+            @Override
+            public boolean checkCanDoRefresh(PtrFrameLayout frame, View content, View header) {
+                return true;
+            }
+
+            @Override
+            public void onRefreshBegin(final PtrFrameLayout frame) {
+                frame.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        // frame.refreshComplete();
+                        simpleRecyclerViewAdapter.insert("Refresh things", 0);
+                        //   ultimateRecyclerView.scrollBy(0, -50);
+                        linearLayoutManager.scrollToPosition(0);
+                        ultimateRecyclerView.mPtrFrameLayout.refreshComplete();
+                        if (mLoadTime % 2 == 0)
+                            changeHeaderHandler.sendEmptyMessageDelayed(1, 500);
+                    }
+                }, 2000);
+            }
+        });
     }
 
 }
